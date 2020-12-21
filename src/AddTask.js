2c9692ecaecb810ad42756f78769b62eddc617aa
İlker Oğuz <ilker.oguz@modanisa.com>
2020-12-21 10:04:24 +0300

    [İlker] Fix eslint errors, Remove force refresh page

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,32 +1,33 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Input } from "reactstrap";
+
 export default class AddTask extends Component {
-  
   render() {
     return (
-      <div>
-        <Form onSubmit={this.props.addTask}>
-          <FormGroup>
-            <Input
-              type="textarea"
-              name="currentTask"
-              id="addTask"
-              placeholder="Add Task Please"
-              onChange={this.props.onChangeHandle}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Input type="select" name="currentSubject" id="selectSubject" onChange={this.props.onChangeHandle}>
-              {this.props.categoryList.map((category)=>( 
-                  <option key={category.id}>{category.categoryName}</option>
-              ))}
-            </Input>
-          </FormGroup>
-          <Button color="primary">
-            Add Task
-          </Button>
-        </Form>
-      </div>
+      <Form onSubmit={this.props.addTask}>
+        <FormGroup>
+          <Input
+            type="textarea"
+            name="currentTask"
+            id="addTask"
+            placeholder="Add Task Please"
+            onChange={this.props.onChangeHandle}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="select"
+            name="currentSubject"
+            id="selectSubject"
+            onChange={this.props.onChangeHandle}
+          >
+            {this.props.categoryList.map((category) => (
+              <option key={category.id}>{category.categoryName}</option>
+            ))}
+          </Input>
+        </FormGroup>
+        <Button color="primary">Add Task</Button>
+      </Form>
     );
   }
 }
